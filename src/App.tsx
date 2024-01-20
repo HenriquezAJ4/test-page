79debb28879534a2a0c31c53163e266adb625b4b
Apolinar Henriquez <henriquez.aj4@gmail.com>
2024-01-19 23:24:20 -0400

    second commit

--- src/App.tsx
+++ src/App.tsx
@@ -16,7 +16,7 @@ function App() {
           <img src={reactLogo} className="logo react" alt="React logo" />
         </a>
       </div>
-      <h1>Algo</h1>
+      <h1>Daniela</h1>
       <div className="card">
         <button onClick={() => setCount((count) => count + 1)}>
           count is {count}
